eabeb384039e060b359333cb7afbb7d0ae743084
Ali200119 <aliit@code.edu.az>
2022-04-15 20:36:31 +0400

    feat: ComicbookRepository (Implement)

---
+++ ConsoleApp/Business/Repositories/ComicbookRepository.cs
@@ -0,0 +1,34 @@
+﻿using DataAccess.Interfaces;
+using Entities.Models;
+using System;
+
+namespace Business
+{
+    public class ComicbookRepository : IRepository<Comicbook>
+    {
+        public bool Create(Comicbook entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(Comicbook entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public System.Collections.Generic.List<Comicbook> GetAll(Predicate<string> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comicbook GetOne(Predicate<int> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(Comicbook entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
