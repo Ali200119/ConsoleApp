4bde61d5724a1d02d0b1519ea50727b5579f5fc9
Ali200119 <aliit@code.edu.az>
2022-04-16 18:29:07 +0400

    feat: IComicbook

---
+++ ConsoleApp/Business/Interfaces/IComicbook.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Interfaces
+{
+    internal interface IComicbook
+    {
+        Comicbook Create(Comicbook comicbook);
+        Comicbook Update(int Id, Comicbook comicbook);
+        Comicbook Delete(int Id);
+        Comicbook GetComicbook(string name);
+    }
+}
--- ConsoleApp/Business/Interfaces/ILibrary.cs
+++ ConsoleApp/Business/Interfaces/ILibrary.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,9 @@ namespace Business.Interfaces
 {
     public interface ILibrary
     {
+        Library Create(Library library);
+        Library Update(int Id, Library library);
+        Library Delete(int Id);
+        Library GetLibrary(string name);
     }
 }
